bf35594c29952583af8dc4b149c32834571f2b1c
gusilva <gusilva@padtec.com.br>
2018-06-18 14:36:00 -0300

    18/06/2018

--- casadocodigo/test/produtosSupertest.js
+++ casadocodigo/test/produtosSupertest.js
@@ -3,11 +3,27 @@ var express = require('../config/express')();
 var request = require('supertest')(express);
 
 describe('#ProdutosControllerNewSupertest', function () {
-    // Nesse não precisa do assert
-    it('#listagem json', function (done) {
-        request.get('/produtos') // Get
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/) // Retorna Json
-            .expect(200, done); // Verifica se o retorno é 200 e finaliza
-    });
+
+  // Nesse não precisa do assert
+  it('#listagem json', function (done) {
+    request.get('/produtos') // Get
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/) // Retorna Json
+    .expect(200, done); // Verifica se o retorno é 200 e finaliza
+  });
+
+  it('#Cadastro de novo produto com dados invalidos', function (done) {
+    request.post('/produtos')
+    .set('Accept', 'application/json')
+    .send({titulo: "", descricao: "Novo livro"})
+    .expect(400, done);
+  });
+
+  it('#Cadastro de novo produto com dados validos', function (done) {
+    request.post('/produtos')
+    .set('Accept', 'application/json')
+    .send({titulo: "Java", descricao: "Novo livro de Java", preco: 20.50})
+    .expect(302, done);
+  });
+
 });
