ac4d7f2d91f995461e8c1560702f187fef9c0783
gusilva <gusilva@padtec.com.br>
2018-06-20 16:49:33 -0300

    Finish

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -40,3 +40,16 @@ app.listen(port, function () {
 http.listen(port, function () {
   console.log('Servidor rodando -> http://%s:%s', 'localhost', port);
 });
+
+
+//Heroko
+/*
+var app = require('./config/express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.set('io',io);
+var porta = process.env.PORT || 3000;
+http.listen(porta,fuhnction(){
+    console.log("Servidor rodando no heroku");
+});
+*/
--- casadocodigo/app/infra/connectionFactory.js
+++ casadocodigo/app/infra/connectionFactory.js
@@ -12,12 +12,23 @@ function createDBConnection() {
     });
   }
 
-  if (process.env.NODE_ENV == 'test') {
+  if (process.env.NODE_ENV === 'test') {
     return mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: 'padtec',
       database: 'casadocodigo_nodejs_test'
+    });
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    var url = process.env.CLEARDB_DATABASE_URL;
+    var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+    return mysql.createConnection({
+      host:grupos[3],
+      user:grupos[1],
+      password:grupos[2],
+      database:grupos[4]
     });
   }
 }
